c0186d1434b96f70c8009c6ff985f38612ef4e11
407-35-kanyapat <54811@yupparaj.ac.th>
2026-06-26 11:35:50 +0700

    Update digital_profile.py

--- digital_profile.py
+++ digital_profile.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.title("My Digital Profile")
+st.title("📍My Digital Profile")
 
-st.header("แนะนำตัว")
+st.header("🙌🏼แนะนำตัว")
 st.write("นางสาว กัญญาพัชร ศักดิ์เสือ")
-st.write("ชื่อเล่น แชต")
+st.write("ชื่อเล่น แชต.🎆")
 st.write("เลขที่ 35 ม.4/7")
 
-st.header("เป้าหมายในอนาคต")
+st.header("🍘เป้าหมายในอนาคต")
 st.write("เป้าหมาย : อยากเรียนเกี่ยวกับด้านอาหาร")
-st.write("สิ่งที่อยากเป็นในอนาคต ; เชฟ")
+st.write("สิ่งที่อยากเป็นในอนาคต ; เชฟ👩🏽‍🍳")
